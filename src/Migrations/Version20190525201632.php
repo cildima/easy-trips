1856dc0e59cfb474737bf6fa481d7daf98816f07
Dmitriy Silitskas <dmitriy.silitckas@mediapark.lt>
2019-05-25 22:18:01 +0200

    Created migration for new Entity

---
+++ src/Migrations/Version20190525201632.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190525201632 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE articles (id INT AUTO_INCREMENT NOT NULL, user_create_id INT NOT NULL, name VARCHAR(255) NOT NULL, preview_text LONGTEXT DEFAULT NULL, preview_image VARCHAR(255) DEFAULT NULL, detail_text LONGTEXT NOT NULL, detail_image VARCHAR(255) DEFAULT NULL, date_create DATETIME NOT NULL, date_update DATETIME NOT NULL, INDEX IDX_BFDD3168EEFE5067 (user_create_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD3168EEFE5067 FOREIGN KEY (user_create_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE articles');
+    }
+}
